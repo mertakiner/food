0a11ceba6a3a25707353cd96ce2cff806369aa18
mert <mac@Merts-iMac.local>
2020-06-10 13:37:41 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import Footer from "./components/layouts/Footer";
+import Meetings from "./components/pages/Meetings";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p> Mert </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container mx-auto">
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/etkinlikler" exact component={Meetings} />
+        </div>
+      </div>
+        <Footer />
+    </Router>
   );
 }
 
