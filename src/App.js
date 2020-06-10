f61741f98863599fba9c7af3b5e19506fb9e9d04
mert <mac@Merts-iMac.local>
2020-06-10 18:19:26 +0300

    HashRouter-2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, HashRouter } from "react-router-dom";
 import Navbar from "./components/layouts/Navbar";
 import About from "./components/pages/About";
 import Home from "./components/pages/Home";
@@ -8,17 +8,19 @@ import Meetings from "./components/pages/Meetings";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container mx-auto">
-          <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
-          <Route path="/etkinlikler" exact component={Meetings} />
+    <HashRouter>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container mx-auto">
+            <Route path="/" exact component={Home} />
+            <Route path="/about" exact component={About} />
+            <Route path="/etkinlikler" exact component={Meetings} />
+          </div>
         </div>
-      </div>
-        <Footer />
-    </Router>
+          <Footer />
+      </Router>
+    </HashRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
       <App />
-    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
